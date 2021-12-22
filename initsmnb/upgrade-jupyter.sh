126383620fe730b3bd24df889ef20a6cbf9ab01d
Verdi March <verdimrc@users.noreply.github.com>
2021-12-22 17:53:50 +0000

    Apply JLab-3+ configs

--- initsmnb/upgrade-jupyter.sh
+++ initsmnb/upgrade-jupyter.sh
@@ -6,13 +6,15 @@ if [[ $FLAVOR != "Amazon Linux 2" ]]; then
     exit 1
 fi
 
-################################################################################
-# IMPLEMENTATION NOTES: upgrade in-place JupyterSystemEnv instead of createing a
-# new conda env for the new Jupyter version, because the original conda
-# environment has sagemaker nbi agents installed (and possibly other pypi stuffs
-# needed to make notebook instance works).
-################################################################################
 
+################################################################################
+# STEP-00: upgrade to JLab-3.x
+#
+# [IMPLEMENTATION NOTES] Upgrade in-place JupyterSystemEnv instead of a new
+# dedicated conda env for the new JLab, because the existing conda environment
+# has sagemaker nbi agents installed (and possibly other pypi stuffs needed to
+# make notebook instance works).
+################################################################################
 CONDA_ENV_DIR=~/anaconda3/envs/JupyterSystemEnv/
 BIN_DIR=$CONDA_ENV_DIR/bin
 
@@ -43,3 +45,111 @@ $BIN_DIR/pip install --no-cache-dir --upgrade "${PKGS[@]}"
 for i in $CONDA_ENV_DIR/share/jupyter/lab/extensions/jupyterlab-{celltags,toc,git}-*.tgz; do
     rm $i
 done
+
+
+################################################################################
+# STEP-01: Apply jlab-3+ configs
+################################################################################
+JUPYTER_CONFIG_ROOT=~/.jupyter/lab/user-settings/\@jupyterlab
+
+# Show traliling space is brand-new since JLab-3.2.0
+# See: https://jupyterlab.readthedocs.io/en/3.2.x/getting_started/changelog.html#id22
+mkdir -p $JUPYTER_CONFIG_ROOT/notebook-extension/
+cat << EOF > $JUPYTER_CONFIG_ROOT/notebook-extension/tracker.jupyterlab-settings
+{
+    // Notebook
+    // @jupyterlab/notebook-extension:tracker
+    // Notebook settings.
+    // **************************************
+    "codeCellConfig": {
+        "rulers": [80, 100],
+        "lineNumbers": true,
+        "showTrailingSpace": true
+    },
+    "markdownCellConfig": {
+        "rulers": [80, 100],
+        "lineNumbers": true,
+        "showTrailingSpace": true
+    },
+    "rawCellConfig": {
+        "rulers": [80, 100],
+        "lineNumbers": true,
+        "showTrailingSpace": true
+    }
+}
+EOF
+
+mkdir -p $JUPYTER_CONFIG_ROOT/fileeditor-extension/
+cat << EOF > $JUPYTER_CONFIG_ROOT/fileeditor-extension/plugin.jupyterlab-settings
+{
+    // Text Editor
+    // @jupyterlab/fileeditor-extension:plugin
+    // Text editor settings.
+    // ***************************************
+    "editorConfig": {
+        "rulers": [80, 100],
+        "codeFolding": true,
+        "lineNumbers": true,
+        "showTrailingSpace": true
+    }
+}
+EOF
+
+# macOptionIsMeta is brand-new since JLab-3.0.
+# See: https://jupyterlab.readthedocs.io/en/3.0.x/getting_started/changelog.html#other
+mkdir -p $JUPYTER_CONFIG_ROOT/terminal-extension/
+cat << EOF > $JUPYTER_CONFIG_ROOT/terminal-extension/plugin.jupyterlab-settings
+{
+    // Terminal
+    // @jupyterlab/terminal-extension:plugin
+    // Terminal settings.
+    // *************************************
+
+    // Font size
+    // The font size used to render text.
+    "fontSize": 11,
+
+    // Theme
+    // The theme for the terminal.
+    "theme": "dark",
+
+    // Treat option as meta key on macOS (new in JLab-3.0)
+    // Option key on macOS can be used as meta key. This enables to use shortcuts such as option + f
+    // to move cursor forward one word
+    "macOptionIsMeta": true
+}
+EOF
+
+# Undo the old "mac-option-is-meta" mechanism designed for jlab<3.0.
+echo "# JLab-3 + macOptionIsMeta deprecates fix-osx-keymap.sh" > ~/.inputrc
+rm ~/.ipython/profile_default/startup/01-osx-jupyterlab-keys.py
+
+# Show command palette on lhs navbar, similar behavior to smnb.
+mkdir -p $JUPYTER_CONFIG_ROOT/apputils-extension/
+cat << EOF > $JUPYTER_CONFIG_ROOT/apputils-extension/palette.jupyterlab-settings
+{
+    // Command Palette
+    // @jupyterlab/apputils-extension:palette
+    // Command palette settings.
+    // **************************************
+
+    // Modal Command Palette
+    // Whether the command palette should be modal or in the left panel.
+    "modal": false
+}
+EOF
+
+# Auto-apply black & isort when saving on notebook editor (but sadly, not on text editor).
+mkdir -p $JUPYTER_CONFIG_ROOT/jupyterlab_code_formatter-extension/
+cat << EOF > $JUPYTER_CONFIG_ROOT/jupyterlab_code_formatter-extension/settings.jupyterlab-settings
+{
+    // Jupyterlab Code Formatter
+    // @ryantam626/jupyterlab_code_formatter:settings
+    // Jupyterlab Code Formatter settings.
+    // **********************************************
+
+    // Auto format config
+    // Auto format code when save the notebook.
+    "formatOnSave": true,
+}
+EOF
