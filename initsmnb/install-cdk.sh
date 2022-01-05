90e861c0fd3942faea5ae593a72eb8355928c476
Verdi March <verdimrc@users.noreply.github.com>
2022-01-05 14:24:52 +0000

    CDK: install latest release of nvm

--- initsmnb/install-cdk.sh
+++ initsmnb/install-cdk.sh
@@ -39,11 +39,12 @@ mkdir -p $NVM_DIR
 # Install nvm to NVM_DIR
 if [[ $(detect_cmd nvm) == "not_detected" ]]; then
     echo "Installing nvm..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+    NVM_VERSION=$(curl -sL https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.name')
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
     [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # Loads nvm
     [[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"  # Loads nvm bash_completion
 fi
-echo "Checking nvm:" `command -v nvm`
+echo "Checking nvm:" `nvm --version`
 
 # Install node.js (use lts version as-per CDK recommendation)
 if [[ $(detect_cmd node -e "console.log('Running Node.js ' + process.version)") == "not_detected" ]]
