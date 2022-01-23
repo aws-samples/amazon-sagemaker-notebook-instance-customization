34ed9d2f0e36cbe114135aa24a1b894cfe4d640f
Verdi March <verdimrc@users.noreply.github.com>
2022-01-23 23:24:25 +0800

    Tone down dark-blues on ipython-8+ traceback

--- initsmnb/fix-ipython.sh
+++ initsmnb/fix-ipython.sh
@@ -4,11 +4,12 @@ echo "Change ipython color scheme on something.__class__ from dark blue (nearly 
 
 mkdir -p ~/.ipython/profile_default/
 
-cat << EOF >> ~/.ipython/profile_default/ipython_config.py
+cat << 'EOF' >> ~/.ipython/profile_default/ipython_config.py
 # See: https://stackoverflow.com/a/48455387
 
 """
-Change default dark blue for "object.__file__" to a more readable color, esp. on dark background.
+Syntax highlighting on Input: Change default dark blue for "object.__file__" to
+a more readable color, esp. on dark background.
 
 Find out the correct token type with:
 
@@ -23,11 +24,26 @@ from pygments.token import Name
 
 c.TerminalInteractiveShell.highlighting_style_overrides = {
     Name.Variable: "#B8860B",
-    Name.Variable.Magic: "#B8860B",   # Unclear why certain ipython prefers this
-    Name.Function: "#6fa8dc",         # For IPython 8+ (tone down dark blue for function name)
+    Name.Variable.Magic: "#B8860B",  # Unclear why certain ipython prefers this
+    Name.Function: "#6fa8dc",        # For IPython 8+ (tone down dark blue for function name)
 }
 
 c.TerminalInteractiveShell.highlight_matching_brackets = True
+
+
+################################################################################
+"""
+Syntax highlighting on traceback: Tone down all dark blues. IPython-8+ has more
+dark blue compared to older versions. Quick test with the following:
+
+>>> import asdf
+
+Unfortunately, `IPython.core.ultratb.VerboseTB.get_records()` hardcoded the
+"default" pygments style, and doesn't seem to provide a way to override unlike
+what Input provides. Hence, let's directly override pygments.
+"""
+from pygments.styles.default import DefaultStyle
+DefaultStyle.styles = {k: v.replace("#0000FF", "#6fa8dc") for k, v in DefaultStyle.styles.items()}
 EOF
 
 
