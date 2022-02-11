262e1392ea943f2bc7f9039a943613e8115bf925
Verdi March <verdimrc@users.noreply.github.com>
2022-02-11 04:04:51 +0000

    Reduce bash start-up time

--- initsmnb/patch-bash-config.sh
+++ initsmnb/patch-bash-config.sh
@@ -43,5 +43,42 @@ alias ll='ls -alF --color=auto'
 export LS_COLORS="di=38;5;39"
 EOF
 
-
 #echo "On a new SageMaker terminal, which uses 'sh' by default, type 'bash -l' (without the quotes)"
+
+###############################################################################
+# A few useful AWS environment variables
+###############################################################################
+# To speed-up shell initialization, locks to setup-time's EC2 instance.
+ACCOUNT=$(aws sts get-caller-identity | grep Account | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]\$//'`"
+
+# Add environment variables
+cat << EOF >> ~/.bashrc
+
+# Non-standard environment variables
+export AWS_ACCOUNT=$ACCOUNT
+export EC2_AVAIL_ZONE=$EC2_AVAIL_ZONE
+
+# AWS CLI's environment variables
+# Fallback to ~/.aws/config
+#export AWS_DEFAULT_REGION=$REGION
+#export AWS_REGION=$REGION
+
+# CDK's environment variables
+export CDK_DEFAULT_ACCOUNT=$ACCOUNT
+export CDK_DEFAULT_REGION=$REGION
+EOF
+
+# Provide reference on how to regenerate the values.
+cat << 'EOF' >> ~/.bashrc
+
+################################################################################
+# NOTE: this noticeably slows down shell initialization by another ~3 second.
+# It's left as a reference in case you want to regenerate the above values.
+################################################################################
+#AWS_ACCOUNT=$(aws sts get-caller-identity | grep Account | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+#EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+#AWS_DEFAULT_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]\$//'`"
+################################################################################
+EOF
