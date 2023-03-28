95268aeea31d733ab035cb4f74d1eb040a2f2d54
Verdi March <verdimrc@users.noreply.github.com>
2023-03-28 14:49:19 +0800

    awscliv2: enable on-partial auto completion

--- initsmnb/install-cli.sh
+++ initsmnb/install-cli.sh
@@ -78,3 +78,4 @@ aws configure set default.s3.max_concurrent_requests 100
 aws configure set default.s3.max_queue_size 10000
 aws configure set default.s3.multipart_threshold 64MB
 aws configure set default.s3.multipart_chunksize 16MB
+aws configure set default.cli_auto_prompt on-partial
